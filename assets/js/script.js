c0f0882cb7ef081d8d8dcbee69c707d7ee304a94
naim68 <shakhawat.naim2@gmail.com>
2023-07-13 18:18:52 +0600

    Carousel update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,44 @@
 //navbar bg color after scroll
-let navBar=document.querySelector("nav");
+let navBar = document.querySelector("nav");
 window.addEventListener("scroll", function () {
-    if (window.pageYOffset > 50) {
-        navBar.classList.add("navbar-bg", "shadow");
-    }else {
-        navBar.classList.remove("navbar-bg", "shadow");
-    }
-})
+  if (window.pageYOffset > 50) {
+    navBar.classList.add("navbar-bg", "shadow");
+  } else {
+    navBar.classList.remove("navbar-bg", "shadow");
+  }
+});
+
+// carousel
+$(".carousel-one").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
+$(".carousel-two").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
